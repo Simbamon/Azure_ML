fcfab98d7434f98614c1e27b0d8d54936cacd20f
Simbamon <sbhan9511@gmail.com>
2024-10-16 19:01:20 +0900

    raydp added

--- data_process/custom_spark/data_processing_src/pyspark_with_ray.py
+++ data_process/custom_spark/data_processing_src/pyspark_with_ray.py
@@ -3,12 +3,18 @@ import ray
 from ray_on_aml.core import Ray_On_AML
 import raydp
 
+def data_processing() -> None:
+    spark = raydp.init_spark(app_name='RayDP spark cluster',
+                         num_executors=2,
+                         executor_cores=2,
+                         executor_memory='4GB')
+
 def testing() -> None:
     print("printing ray version...")
     ray_version = subprocess.run("ray --version", shell=True, capture_output=True)
 
 def main() -> None:
-    # Your data processing function goes here
+    # Your data processing function goes herewd
     testing()
 
 if __name__ == "__main__":
