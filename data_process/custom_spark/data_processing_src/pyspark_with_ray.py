f0f7bbdeb02425135c943f8dd45910abc2376b2a
Simbamon <sbhan9511@gmail.com>
2024-11-02 18:35:30 +0900

    spark read added

--- data_process/custom_spark/data_processing_src/pyspark_with_ray.py
+++ data_process/custom_spark/data_processing_src/pyspark_with_ray.py
@@ -4,7 +4,7 @@ from ray_on_aml.core import Ray_On_AML
 import raydp
 import argparse
 
-def data_processing() -> None:
+def data_processing(input_path: str) -> None:
     spark = raydp.init_spark(app_name='RayDP spark cluster',
                          num_executors=2,
                          executor_cores=2,
@@ -16,16 +16,21 @@ def data_processing() -> None:
     data = [("sample1", 10), ("sample2", 15), ("sample3", 20)]
     sample_df = spark.createDataFrame(data, ["sample", "number"])
 
+    data_from_spark = spark.read.format('parquet').option('recursiveFileLookup', 'true').option('header', 'true').load(input_path)
+
 def testing() -> None:
     print("printing ray version...")
     ray_version = subprocess.run("ray --version", shell=True, capture_output=True)
 
 def main() -> None:
-    # Your data processing function goes herewd
     testing()
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--input_path", help="input path")
+    args = parser.parse_args()
+    input_path = args.input_path
+
+    data_processing(input_path)
 
 if __name__ == "__main__":
     ray_on_aml = Ray_On_AML()
